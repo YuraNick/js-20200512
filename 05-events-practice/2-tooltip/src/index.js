052335a141428b228b0cd98f8d11da6047d498bd
yarovikov <505-565@mail.ru>
2020-06-03 22:58:22 +0500

    practice 5 task 2 (tooltip) has been done

--- 05-events-practice/2-tooltip/src/index.js
+++ 05-events-practice/2-tooltip/src/index.js
@@ -1,4 +1,53 @@
 class Tooltip {
+    static onlyInstance = null;
+    tooltipElement;
+
+    pointerOverEvent = (event) => {
+        const tooltipElement = event.target.closest('[data-tooltip]');
+        if (tooltipElement) {
+            this.tooltipElement.innerHTML = tooltipElement.dataset.tooltip;
+            document.body.append(this.tooltipElement);
+
+            tooltipElement.addEventListener('mousemove', this.mouseMoveEvent);
+        }
+    }
+
+    mouseMoveEvent = (event) => {
+        this.tooltipElement.style.left = event.clientX + 5 + "px";
+        this.tooltipElement.style.top = event.clientY + 5 + "px";
+    }
+
+    pointerOutEvent = (event) => {
+        const tooltipElement = event.target.closest('[data-tooltip]');
+        if (tooltipElement) {
+            this.tooltipElement.innerHTML = '';
+            
+            this.tooltipElement.remove();
+            event.target.removeEventListener('mousemove', this.mouseMoveEvent)
+        }
+    }
+
+
+    constructor() {
+        if (! Tooltip.onlyInstance) {
+            Tooltip.onlyInstance = this;
+        } else {
+            return Tooltip.onlyInstance;
+        }
+    }
+
+    initialize() {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = this.tooltipTemplate;
+        this.tooltipElement = wrapper.firstElementChild;
+
+        document.body.addEventListener('pointerover', this.pointerOverEvent);
+        document.body.addEventListener('pointerout', this.pointerOutEvent);
+    }
+
+    get tooltipTemplate() {
+        return `<div class="tooltip"></div>`;
+    }
 
 }
 

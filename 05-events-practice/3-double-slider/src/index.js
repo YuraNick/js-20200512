a12d4a9a3adb5c1b47bc6e65ea417cdc6f1903e4
yarovikov <505-565@mail.ru>
2020-06-05 11:25:45 +0500

    practice 5 task 3 (double-slider) has been done

--- 05-events-practice/3-double-slider/src/index.js
+++ 05-events-practice/3-double-slider/src/index.js
@@ -2,90 +2,115 @@ export default class DoubleSlider {
     element;
     subElements;
     setup;
+    min;
+    max;
     selected;
+    rangeSelectedEvent;
 
     mouseDown = (event) => {
         const element = event.target.closest('[data-element]');
-        const dataElement = (element) ? element.dataset.element : '';
+        const elementName = (element) ? element.dataset.element : '';
         
-        if (dataElement === 'thumbLeft' || dataElement === 'thumbRight') {
-            this.selected.innerProperty = this.subElements.inner.getBoundingClientRect();
+        if (elementName === 'thumbLeft' || elementName === 'thumbRight') {
+            // так тесты не проходят, а в браузере нормально работает:
+            // this.selected.innerProperty = this.subElements.inner.getBoundingClientRect();
+            this.selected.innerProperty = this.getBoundingClientRect(this.subElements.inner);
+
             this.selected.shiftX = event.clientX - element.getBoundingClientRect().left;
 
-            if (dataElement === 'thumbLeft') {
-                this.selected.dragZone = {
-                    left : this.selected.innerProperty.left,
-                    right : this.subElements.thumbRight.getBoundingClientRect().left
-                };
-            } else {
-                this.selected.dragZone = {
-                    left : this.subElements.thumbLeft.getBoundingClientRect().right,
-                    right : this.selected.innerProperty.right
-                };
+            switch (elementName) {
+                case 'thumbLeft':
+                    this.selected.dragZone = {
+                        left : this.selected.innerProperty.left,
+                        // при right : this.subElements.thumbRight.getBoundingClientRect().left, тесты не проходят, а в браузере нормально работает:
+                        right : this.selected.innerProperty.left + ((100 - this.selected.thumbRight) * this.selected.innerProperty.width / 100),
+                    };
+                    break;
+
+                case 'thumbRight':
+                    this.selected.dragZone = {
+                        // при left : this.subElements.thumbLeft.getBoundingClientRect().right, тесты не проходят, а в браузере нормально работает:
+                        left : this.selected.innerProperty.left + (this.selected.thumbLeft * this.selected.innerProperty.width / 100),
+                        right : this.selected.innerProperty.right
+                    };
             }
 
-            this.selected.dataElement = dataElement;
+            this.selected.elementName = elementName;
 
-            document.addEventListener('mousemove', this.mouseMove);
-            document.addEventListener('mouseup', this.mouseUp, {once:true});
+            document.addEventListener('pointermove', this.mouseMove);
+            document.addEventListener('pointerup', this.mouseUp, {once:true});
         }
     };
 
     mouseMove = (event) => {
-        let newX = event.pageX - this.selected.shiftX;
+        let newX = event.clientX - this.selected.shiftX;
 
         if (newX < this.selected.dragZone.left) {
             newX = this.selected.dragZone.left;
-        }
-
-        if (newX > this.selected.dragZone.right) {
+        } else if (newX > this.selected.dragZone.right) {
             newX = this.selected.dragZone.right;
         }
 
         const newPercent = 100 * (newX - this.selected.innerProperty.left) / this.selected.innerProperty.width;
-        const newValue = Math.round(this.setup.min + (newPercent * this.setup.range / 100));
+        const newValue = this.setup.min + (newPercent * this.setup.range / 100);
 
-        switch (this.selected.dataElement) {
+        switch (this.selected.elementName) {
             case 'thumbLeft' :
-                this.setLeftElementStyle({thumbLeft: newPercent, from : newValue});
+                this.selected.thumbLeft = newPercent;
+                this.selected.from = newValue;
+                this.setLeftElementStyle(this.selected);
                 break;
+                
             case 'thumbRight':
-                this.setRightElementStyle({thumbRight: 100 - newPercent, to : newValue});
+                this.selected.thumbRight = 100 - newPercent;
+                this.selected.to = newValue;
+                this.setRightElementStyle(this.selected);
         }
     };
 
-    mouseUp = (event) => {
-        document.removeEventListener('mousemove', this.mouseMove);
+    mouseUp = () => {
+        this.element.dispatchEvent(this.rangeSelectedEvent);
+        document.removeEventListener('pointermove', this.mouseMove);
     } 
 
     constructor(
         {
             min = 0,
             max = 100,
-            selected = { from: 11, to: 100 },
+            selected = { from: min, to: max },
             formatValue = value => value + '%'
         } = {}
     ){
+        this.min = min;
+        this.max = max;
+        
         this.setup = {
             min,
             max,
             range : max - min,
             formatValue
         };
+        
         this.selected = this.getBeginSelected(selected);
 
-        this.render(document.body);
+        this.render();
     }
 
-    render (parent) {
+    render () {
         const wrapper = document.createElement('div');
         wrapper.innerHTML = this.template;
 
         this.element = wrapper.firstElementChild;
         this.subElements = this.getSubElements(this.element);
-        this.setElementStyle();
+        this.setLeftElementStyle(this.selected);
+        this.setRightElementStyle(this.selected);
 
-        this.element.addEventListener('mousedown', this.mouseDown);
+        this.element.addEventListener('pointerdown', this.mouseDown);
+
+        this.rangeSelectedEvent = new CustomEvent("range-select", {
+            detail: {from: this.selected.from, to: this.selected.to},
+            bubbles: true
+        });
     }
 
     getSubElements (element) {
@@ -112,45 +137,61 @@ export default class DoubleSlider {
         `;
     }
 
-    setElementStyle() {
-        const objValues = {
-            thumbLeft : 100 * this.selected.left / this.setup.range,
-            thumbRight : 100 * this.selected.right / this.setup.range,
-            from : this.setup.formatValue(this.selected.from),
-            to : this.setup.formatValue(this.selected.to)
-        };
-
-        this.setLeftElementStyle(objValues);
-        this.setRightElementStyle(objValues);
-    }
-
-    setLeftElementStyle({thumbLeft, from}) {
-        this.subElements.from.innerText = from;
+    setLeftElementStyle ({thumbLeft, from}) {
+        this.subElements.from.textContent = this.setup.formatValue(Math.round(from));
         this.subElements.thumbLeft.style.left = `${thumbLeft}%`;
         this.subElements.progress.style.left = `${thumbLeft}%`;
     }
     
-    setRightElementStyle({thumbRight, to}) {
-        this.subElements.to.innerText = to;
+    setRightElementStyle ({thumbRight, to}) {
+        this.subElements.to.textContent = this.setup.formatValue(Math.round(to));
         this.subElements.thumbRight.style.right = `${thumbRight}%`;
         this.subElements.progress.style.right = `${thumbRight}%`;
     }
 
     getBeginSelected ({from = 0, to = 0}) {
-        if (from < this.setup.min) from = this.setup.min;
-        if (to > this.setup.max) to = this.setup.max;
-        if (to < from) to = from;
+        let left = from - this.setup.min;
+        let right = this.setup.range - to + this.setup.min;
+        
+        if (left < 0) { left = 0 };
+        if (right < 0) { right = 0 };
 
         return {
           from, 
           to, 
-          left : from,
-          right : this.setup.range - to,
+          left : left,
+          right : right,
+          thumbLeft : 100 * left / this.setup.range,
+          thumbRight : 100 * right / this.setup.range,
           shiftX : 0,
-          dataElement : '',
+          elementName : '',
           dragZone : {},
           innerProperty : {}
         };
+    }
+
+    getBoundingClientRect (element) {
+        if (element.getBoundingClientRect().right) {
+            return element.getBoundingClientRect();
+        }
+
+         // добавлено для тестов
+        return {
+            left : element.getBoundingClientRect().left,
+            width : element.getBoundingClientRect().width,
+            right : element.getBoundingClientRect().left + element.getBoundingClientRect().width
+        }
+    }
+
+    removeEvents() {
+        document.removeEventListener('pointermove', this.mouseMove);
+        document.removeEventListener('pointerup', this.mouseUp, {once:true});
+        this.element.removeEventListener('pointerdown', this.mouseDown);
+    }
+
+    destroy() {
+        this.removeEvents();
+        this.element.remove();
     }
 
 }
